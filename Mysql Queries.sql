3affd50b89e4a88cb1f5bae77719c97636dee073
ahsanshahzad666 <ahsanshahzadds66@gmail.com>
2025-07-17 13:57:56 +0500

    Update Mysql Queries.sql

--- Mysql Queries.sql
+++ Mysql Queries.sql
@@ -1,1 +1,23 @@
+-- Walmart Project Queries - MySQL
+
+SELECT * FROM walmart;
+
+
+-- Count total records
+SELECT COUNT(*) FROM walmart;
+
+
+
+-- Count payment methods and number of transactions by payment method
+select payment_method , count(*) as total_transcations from walmart
+group by payment_method;
+
+
+-- Count distinct branches
+SELECT COUNT(DISTINCT branch) FROM walmart;
+
+
+-- find min and max quantity 
+select max(quantity) from walmart ; 
+select min(quantity) from walmart ;
 
